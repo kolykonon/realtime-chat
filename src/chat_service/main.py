5bd832de74ef20d7f9846ccaa7f2a41f1c04172f
kolykonon <kononikhinnikolay@gmail.com>
2026-09-19 17:37:46 +0300

    add session middleware

--- src/chat_service/main.py
+++ src/chat_service/main.py
@@ -1,10 +1,14 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-
+from starlette.middleware.sessions import SessionMiddleware
 
 from chat_service.db.base import Base
 from chat_service.db.dep import engine
+from chat_service.core.config import get_settings
+
+settings = get_settings()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +17,14 @@ async def lifespan(app: FastAPI):
     Base.metadata.drop_all(engine)
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=settings.security_settings.secret_key,
+    https_only=True,
+    same_site='lax'
+)
+
 
 @app.get('/health')
 def health() -> dict[str,str]:
