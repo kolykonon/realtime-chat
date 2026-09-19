500dc26050d89048ef036901f0d5c86005c2a5ef
kolykonon <kononikhinnikolay@gmail.com>
2026-09-19 17:36:09 +0300

    set up auth service with register user method

---
+++ src/chat_service/services/auth_service.py
@@ -0,0 +1,19 @@
+from fastapi import Depends, HTTPException,status
+
+from chat_service.schemas.user import UserCreate
+from chat_service.services.user_service import get_user_service, UserService
+from chat_service.core.security import hash_password
+
+
+class AuthService:
+    def __init__(self, user_service: UserService = Depends(get_user_service)) -> None:
+        self.user_service = user_service
+
+    async def register_user(self,user: UserCreate) -> None:
+        existed = await self.user_service.get_user(user.username)
+        if existed is not None:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists")
+        user.password = hash_password(user.password)
+        await self.user_service.create_user(user)
+
+
