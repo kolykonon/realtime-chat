5cdfb3bbbffcba25bfe904bc5d3ce116223171d8
kolykonon <kononikhinnikolay@gmail.com>
2026-09-24 11:31:11 +0300

    organize imports and realize user_service class

--- src/chat_service/services/user_service.py
+++ src/chat_service/services/user_service.py
@@ -1,29 +1,32 @@
-from typing import Optional
+from typing import Annotated
 
-from fastapi import Depends, HTTPException
-from sqlalchemy import select
-from sqlalchemy.orm import Session
+from fastapi import Depends
 
-from chat_service.db.dep import get_db
-from chat_service.db.models.user import User
-from chat_service.schemas.user import UserCreate
-
-
-class UserService:
-    def __init__(self, session: Session = Depends(get_db)):
-        self.session = session
-
-    async def get_user(self, username: str, db: Session = Depends(get_db)) -> Optional[User]:
-        query = select(User).where(User.username == username)
-        user = await self.session.execute(query).scalar_one_or_none()
-        return user | None
-
-    async def create_user(self, user: UserCreate, session: Session = Depends(get_db)):
-        self.session.add(user)
-        self.session.commit()
-        return user
+from chat_service.core.security import hash_password
+from chat_service.db.models import User
+from chat_service.db.repositories.user_repo import UserRepo, UserRepoDep
+from chat_service.schemas.user import UserCreate, UserCreateDB, UserRead, UserUpdate
+from chat_service.services.base_service import BaseService
 
 
-def get_user_service(session: Session = Depends(get_db)):
-    return UserService(session)
+class UserService(BaseService[User, UserCreate, UserUpdate, UserRead, UserRepo]):
+    def __init__(self, repo: UserRepo):
+        super().__init__(repo)
 
+    async def create(self, data: UserCreate) -> UserRead:
+        existed = await self.repo.get_by_username(data.username)
+        if existed:
+            raise ValueError("Username already exists")
+        user = await self.repo.create(
+            UserCreateDB(username=data.username, hashed_password=hash_password(data.password))
+        )
+        return UserRead.model_validate(user)
+
+    async def get_user_by_username(self, username: str) -> User | None:
+        return await self.repo.get_by_username(username)
+
+
+def get_user_service(repo: UserRepoDep) -> UserService:
+    return UserService(repo)
+
+UserServiceDep = Annotated[UserService, Depends(get_user_service)]
