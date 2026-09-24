15643cbc6cb58bf7bbdfbbe55d2a9baa8a41b8a2
kolykonon <kononikhinnikolay@gmail.com>
2026-09-24 11:18:07 +0300

    rewrite legacy syntax to more modern and change create function: made it to return ModelType

--- src/chat_service/db/repositories/base_repo.py
+++ src/chat_service/db/repositories/base_repo.py
@@ -1,5 +1,4 @@
 from collections.abc import Sequence
-from typing import TypeVar
 
 from pydantic import BaseModel
 from sqlalchemy import select
@@ -7,12 +6,8 @@ from sqlalchemy import select
 from chat_service.db.base import Base
 from chat_service.db.dep import SessionDep
 
-ModelType = TypeVar("ModelType")
-CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
-UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
-ResponseSchemaType = TypeVar("ResponseSchemaType", bound=BaseModel)
 
-class BaseRepo[ModelType: Base, CreateSchemaType: BaseModel, UpdateSchemaType: BaseModel, ResponseSchemaType: BaseModel]:
+class BaseRepo[ModelType: Base, CreateSchemaType: BaseModel, UpdateSchemaType: BaseModel]:
     def __init__(self, session: SessionDep,model: type[ModelType]) -> None:
         self.session = session
         self.model = model
@@ -30,15 +25,14 @@ class BaseRepo[ModelType: Base, CreateSchemaType: BaseModel, UpdateSchemaType: B
         return result.scalars().all()
 
     async def get_one(self, id: int) -> ModelType | None:
-        query = select(self.model).where(self.model.id == id)  # type: ignore[attr-defined] ругается пайрайт, хотя работает
-        result = await self.session.execute(query)
-        return result.scalar_one_or_none()
+        return await self.session.get(self.model, id)
 
-    async def create(self, create_schema: CreateSchemaType) -> None:
+    async def create(self, create_schema: CreateSchemaType) -> ModelType:
         obj = self.model(**create_schema.model_dump())
         self.session.add(obj)
         await self.session.commit()
         await self.session.flush()
+        return obj
 
     async def update(self, obj: ModelType, update_schema: UpdateSchemaType) -> None:
         for attribute, value in update_schema.model_dump(exclude_unset=True).items():
@@ -49,4 +43,3 @@ class BaseRepo[ModelType: Base, CreateSchemaType: BaseModel, UpdateSchemaType: B
         await self.session.delete(obj)
         await self.session.commit()
         await self.session.flush()
-
